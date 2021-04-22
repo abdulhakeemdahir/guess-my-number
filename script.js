c2ba64a36e79dc1abd5544384d66ff80e55b15c0
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-04-22 05:56:23 -0700

    event listener for clicking guess

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
-document.querySelector(".message").textContent;
 
-document.querySelector(".message").textContent = "Correct Number!";
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-document.querySelector(".guess").value = 5;
+document.querySelector(".check").addEventListener("click", function () {
+	const guess = Number(document.querySelector(".guess").value);
+	console.log(guess);
+
+	if (!guess) {
+		document.querySelector(".message").textContent = "No Number!";
+	}
+});
