0cc5cd7efecf1fdad2a9f2d8fa5ce60aa63ba98b
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-04-22 05:41:00 -0700

    querySelector practice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-'use strict';
+"use strict";
+document.querySelector(".message").textContent;
+
+document.querySelector(".message").textContent = "Correct Number!";
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+document.querySelector(".guess").value = 5;
